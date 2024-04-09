6c1772d32774097b3dadebb507692db53b05789b
Diego Huertas <dhuertasg@ucenfotec.ac.cr>
2024-04-08 22:36:14 -0600

    Logica para calcular las horas trabajadas

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,6 @@ module com.example.salary_project_javafx {
     requires javafx.fxml;
     requires java.sql;
 
-
     opens com.example.salary_project_javafx to javafx.fxml;
     exports com.example.salary_project_javafx;
 }
